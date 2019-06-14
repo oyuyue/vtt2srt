f3f76ab3a8a8f637118782fe1311ea4dda0ceb42
pengweng <peng.weng@highsoft.ltd>
2019-06-14 10:16:26 +0800

    add node env, rename

--- vtt2srt.js
+++ vtt2srt.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const fs = require('fs-extra');
-const walk = require('klaw');
-const commander = require('commander');
-const inquirer = require('inquirer');
-const signale = require('signale');
+#!/usr/bin/env node
 
-const { version } = require('./package.json')
+const path = require("path");
+const fs = require("fs-extra");
+const walk = require("klaw");
+const commander = require("commander");
+const inquirer = require("inquirer");
+const signale = require("signale");
+
+const { version } = require("./package.json");
 
 const interactive = new signale.Signale({ interactive: true });
 
 commander
   .version(version)
-  .option('-d, --delete', '删除原文件')
-  .on('--help', () => {
+  .option("-d, --delete", "删除原文件")
+  .on("--help", () => {
     console.log();
-    console.log('Examples:');
-    console.log('  $ vtt2srt ./subtitles');
-    console.log('  $ vtt2srt -d a.vtt');
+    console.log("Examples:");
+    console.log("  $ vtt2srt ./subtitles");
+    console.log("  $ vtt2srt -d a.vtt");
   })
   .parse(process.argv);
 
 async function vtt2srt(target, { keep = true }) {
   const data = fs
-    .readFileSync(target, 'utf-8')
-    .replace(/^WEBVTT/g, '')
-    .replace(/(\d\d:\d\d)\.(\d\d\d)\b/g, '$1,$2')
-    .replace(/(\n|\s)(\d\d:\d\d,\d\d\d)(\s|\n)/g, '$100:$2$3')
+    .readFileSync(target, "utf-8")
+    .replace(/^WEBVTT/g, "")
+    .replace(/(\d\d:\d\d)\.(\d\d\d)\b/g, "$1,$2")
+    .replace(/(\n|\s)(\d\d:\d\d,\d\d\d)(\s|\n)/g, "$100:$2$3")
     .trim()
     .split(/(?:\r\n\r\n|\n\n|\r\r)/g)
     .map((piece, i) => `${i + 1}\n${piece}\n\n`)
-    .join('');
+    .join("");
 
   fs.writeFileSync(`${target.slice(0, -4)}.srt`, data);
 
@@ -41,15 +43,15 @@ async function run() {
   if (!target) {
     const ans = await inquirer.prompt([
       {
-        type: 'confirm',
-        name: 'do',
-        message: '转换当前目录下的所有 .vtt 为 .srt 文件?',
+        type: "confirm",
+        name: "do",
+        message: "转换当前目录下的所有 .vtt 为 .srt 文件?",
         default: false
       }
     ]);
 
     if (!ans.do) return;
-    target = './';
+    target = "./";
   }
   target = path.resolve(process.cwd(), target);
 
@@ -66,20 +68,20 @@ async function run() {
   };
 
   if (stat.isFile()) {
-    if (!target.endsWith('.vtt')) return signale.error('文件必须是 .vtt 文件');
+    if (!target.endsWith(".vtt")) return signale.error("文件必须是 .vtt 文件");
 
     try {
       interactive.await(target);
       await vtt2srt(target, options);
-      interactive.success('转换完成！');
+      interactive.success("转换完成！");
     } catch (error) {
       interactive.fatal(error);
     }
   } else if (stat.isDirectory()) {
     const files = [];
     walk(target)
-      .on('data', ({ path: p }) => p && p.endsWith('.vtt') && files.push(p))
-      .on('end', async () => {
+      .on("data", ({ path: p }) => p && p.endsWith(".vtt") && files.push(p))
+      .on("end", async () => {
         const len = files.length;
         if (len === 0)
           return signale.error(`目录下没有 .vtt 文件 -> ${target}`);
@@ -87,7 +89,7 @@ async function run() {
         const errors = [];
 
         for (let i = 0; i < len; i++) {
-          interactive.await('[%d/%d] - %s', i + 1, len, files[i]);
+          interactive.await("[%d/%d] - %s", i + 1, len, files[i]);
           try {
             await vtt2srt(files[i], options);
           } catch (error) {
@@ -96,10 +98,10 @@ async function run() {
           }
         }
 
-        interactive.success('[%d/%d] - 全部转换完成', len, len);
+        interactive.success("[%d/%d] - 全部转换完成", len, len);
 
         if (errors.length > 0) {
-          signale.debug('以下文件转换出错 >>>>>>>>>>>>>>');
+          signale.debug("以下文件转换出错 >>>>>>>>>>>>>>");
           console.log();
           errors.forEach(e => {
             signale.error(e);
@@ -107,7 +109,7 @@ async function run() {
         }
       });
   } else {
-    signale.error('非法资源');
+    signale.error("非法资源");
   }
 }
 
